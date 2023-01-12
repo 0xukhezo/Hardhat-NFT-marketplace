97d8e6692af735a85dbfd0490885586ca9e49ef8
0xukhezo <ukhezo.web3@gmail.com>
2023-01-12 13:34:20 +0100

    feat:Add cancel item script

--- scripts/cancel-item.js
+++ scripts/cancel-item.js
@@ -1,9 +1,9 @@
 const { network, deployments, ethers } = require("hardhat")
 const { moveBlocks } = require("../utils/move-blocks")
 
-const PRICE = ethers.utils.parseEther("0.1")
+const TOKEN_ID = 0
 
-async function mintAndList() {
+async function cancel() {
     const accounts = await ethers.getSigners()
     const deployer = accounts[0]
     const nftMarketplaceContractAddress = await deployments.fixture([
@@ -15,30 +15,25 @@ async function mintAndList() {
         nftMarketplaceContractAddress.NftMarketplace.address,
         deployer
     )
-
-    let basicNft = await ethers.getContractAt(
-        "BasicNft",
+    const basicNft = await ethers.getContractAt(
+        "BasicNftTwo",
         basicNftContractAddress.BasicNft.address,
         deployer
     )
+    console.log("Canceling NFT...")
+    const cancelTx = await nftMarketplace.cancelListing(
+        basicNft.address,
+        TOKEN_ID
+    )
+    await cancelTx.wait(1)
+    console.log("NFT canceled...")
 
-    console.log("Minting NFT...")
-    const mintTx = await basicNft.mintNft()
-    const mintTxReceipt = await mintTx.wait(1)
-    const tokenId = mintTxReceipt.events[0].args.tokenId
-    console.log("Approving NFT...")
-    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
-    await approvalTx.wait(1)
-    console.log("Listing NFT...")
-    const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
-    await tx.wait(1)
-    console.log("NFT Listed!")
     if (network.config.chainId == 31337) {
         await moveBlocks(1, (sleepAmount = 1000))
     }
 }
 
-mintAndList()
+cancel()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error)
