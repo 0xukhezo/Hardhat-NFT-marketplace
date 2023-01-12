871bdb2cff70ac50a21a2ac5d3a45def7754287d
0xukhezo <ukhezo.web3@gmail.com>
2023-01-12 12:30:00 +0100

    feat:Add script to mint and list items

---
+++ scripts/mint-and-list-item.js
@@ -0,0 +1,43 @@
+const { network, deployments, ethers } = require("hardhat")
+
+const PRICE = ethers.utils.parseEther("0.1")
+
+async function mintAndList() {
+    const accounts = await ethers.getSigners()
+    const deployer = accounts[0]
+    const nftMarketplaceContractAddress = await deployments.fixture([
+        "NftMarketplace",
+    ])
+    const basicNftContractAddress = await deployments.fixture(["basicNft"])
+    const nftMarketplace = await ethers.getContractAt(
+        "NftMarketplace",
+        nftMarketplaceContractAddress.NftMarketplace.address,
+        deployer
+    )
+    const randomNumber = Math.floor(Math.random() * 2)
+
+    const basicNft = await ethers.getContractAt(
+        "BasicNft",
+        basicNftContractAddress.BasicNft.address,
+        deployer
+    )
+
+    console.log("Minting NFT...")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    console.log("Approving NFT...")
+    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
+    await approvalTx.wait(1)
+    console.log("Listing NFT...")
+    const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
+    await tx.wait(1)
+    console.log("NFT Listed!")
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
